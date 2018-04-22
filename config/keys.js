735d692c595e1d5d0c45316f094480901201170f
Faxiang Li <lifa0917@gmail.com>
2018-04-22 09:20:41 -0400

    handle auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI : 'mongodb://lifa24:lifaSean24@ds157873.mlab.com:57873/storybooks-dev',
+  googleClientID : '1002856857907-hv462m8c87vllkofjoh7clthukc6098k.apps.googleusercontent.com',
+  googleClientSecret : '518RHvZZEMycdHCPK5hidBd7'
+}
