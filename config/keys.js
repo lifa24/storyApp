54f2f08d529274d61d4fa1f3bc6dd9a5ccfd7e52
Faxiang Li <lifa0917@gmail.com>
2018-04-23 14:21:10 -0400

    Version 1.0.0 alpha done

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI : 'mongodb://lifa24:lifaSean24@ds157873.mlab.com:57873/storybooks-dev',
-  googleClientID : '1002856857907-hv462m8c87vllkofjoh7clthukc6098k.apps.googleusercontent.com',
-  googleClientSecret : '518RHvZZEMycdHCPK5hidBd7'
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
 }
