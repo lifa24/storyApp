92282f205189b0a514812054feecbb9562177368
Faxiang Li <lifa0917@gmail.com>
2018-04-22 21:44:31 -0400

    Started UI

--- app.js
+++ app.js
@@ -1,21 +1,57 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+//load user model
+require('./models/User');
 
 //Passport config
 require('./config/passport')(passport);
 
 //load routes
+const index= require('./routes/index');
 const auth= require('./routes/auth');
+
+//Load keys
+const keys = require('./config/keys');
+
+//Mongoose Connect
+mongoose.connect(keys.mongoURI)
+  .then(()=>console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
-app.get('/',(req,res)=>{
-  res.send('It works');
+//Handlebars Middleware
+app.engine('handlebars',exphbs({
+  defaultLayout : 'main'
+})); 
+app.set('view engine','handlebars');
+
+app.use(cookieParser());
+app.use(session({
+  secret : 'secret',
+  resave : false,
+  saveUninitialized : false
+}));
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Set global vars
+app.use((req,res,next)=>{
+  res.locals.user = req.user || null;
+  next();
 });
 
 //Use Routes
+app.use('/',index);
 app.use('/auth',auth);
+
 
 const port = process.env.PORT || 5000;
 
--- routes/auth.js
+++ routes/auth.js
@@ -11,4 +11,18 @@ passport.authenticate('google',{
     res.redirect('/dashboard');
   });
 
+  router.get('/verify',(req,res)=>{
+    if(req.user){
+      console.log(req.user);
+    }else{
+      console.log('Not Auth');
+    }
+  });
+
+  //logout route
+  router.get('/logout',(req,res)=>{
+    req.logout();
+    res.redirect('/');
+  });
+
 module.exports = router;
